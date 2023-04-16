347d405e4794b6ffc67656e0cfaf82521a4171a4
MonroeOmega <warwar4774@gmail.com>
2023-04-16 21:14:38 +0300

    Filled the common template

--- src/main/java/com/example/swordo/controllers/HomeController.java
+++ src/main/java/com/example/swordo/controllers/HomeController.java
@@ -1,4 +1,17 @@
 package com.example.swordo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
 public class HomeController {
+
+    @GetMapping()
+    public String home(Model model){
+        return "town";
+    }
+
 }
